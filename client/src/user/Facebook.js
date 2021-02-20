25f8dcfed6d42866587417b696f5996b3fe9df0d
Mithun raam <mithunmaha1999@gmail.com>
2021-02-21 03:10:03 +0530

    last

--- client/src/user/Facebook.js
+++ client/src/user/Facebook.js
@@ -1,41 +1,38 @@
-import React from "react";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
-import axios from "axios";
-require("dotenv").config();
-const Facebook = ({ informParent }) => {
-    const responseFacebook = (response) => {
+import React from 'react';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import axios from 'axios';
+require('dotenv').config();
+
+const Facebook = ({informParent}) => {
+    const responseFacebook = response => {
         console.log(response);
         axios({
-                method: "POST",
-                url: `${process.env.REACT_APP_API_URL}/facebook-login`,
-                data: { userID: response.userID, accessToken: response.accessToken },
-            })
-            .then((response) => {
-                console.log("FACEBOOK SIGNIN SUCCESS", response.data);
+            method: 'POST',
+            url: `${process.env.REACT_APP_API_URL}/facebook-login`,
+            data: { userID: response.userID, accessToken: response.accessToken }
+        })
+            .then(response => {
+                console.log('FACEBOOK SIGNIN SUCCESS', response.data);
                 // inform parent component
                 informParent(response.data);
             })
-            .catch((error) => {
-                console.log("FACEBOOK SIGNIN ERROR", error.response);
+            .catch(error => {
+                console.log('FACEBOOK SIGNIN ERROR', error.response);
             });
     };
-    return ( <
-        div className = "pb-3" >
-        <
-        FacebookLogin appId = { `${process.env.REACT_APP_FACEBOOK_APP_ID}` }
-        autoLoad = { false }
-        callback = { responseFacebook }
-        render = {
-            (renderProps) => ( <
-                button onClick = { renderProps.onClick }
-                className = "btn btn-primary btn-lg btn-block" >
-                <
-                i className = "fab fa-facebook pr-2" > < /i> Login with Facebook{" "} <
-                /button>
-            )
-        }
-        />{" "} <
-        /div>
+    return (
+        <div className="pb-3">
+            <FacebookLogin
+                appId={`${process.env.REACT_APP_FACEBOOK_APP_ID}`}
+                autoLoad={false}
+                callback={responseFacebook}
+                render={renderProps => (
+                    <button onClick={renderProps.onClick} className="btn btn-primary btn-lg btn-block">
+                        <i className="fab fa-facebook pr-2"></i> Login with Facebook
+                    </button>
+                )}
+            />
+        </div>
     );
 };
 
--- client/src/user/Google.js
+++ client/src/user/Google.js
@@ -1,44 +1,43 @@
-import React from "react";
-import GoogleLogin from "react-google-login";
-import axios from "axios";
-require("dotenv").config();
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import axios from 'axios';
+require('dotenv').config();
 
-const Google = ({ informParent = (f) => f }) => {
-    const responseGoogle = (response) => {
+const Google = ({ informParent = f => f }) => {
+    const responseGoogle = response => {
         console.log(response.tokenId);
         axios({
-                method: "POST",
-                url: `${process.env.REACT_APP_API_URL}/google-login`,
-                data: { idToken: response.tokenId },
-            })
-            .then((response) => {
-                console.log("GOOGLE SIGNIN SUCCESS", response.data);
+            method: 'POST',
+            url: `${process.env.REACT_APP_API_URL}/google-login`,
+            data: { idToken: response.tokenId }
+        })
+            .then(response => {
+                console.log('GOOGLE SIGNIN SUCCESS', response.data);
                 // inform parent component
                 informParent(response.data);
             })
-            .catch((error) => {
-                console.log("GOOGLE SIGNIN ERROR", error.response);
+            .catch(error => {
+                console.log('GOOGLE SIGNIN ERROR', error.response);
             });
     };
-    return ( <
-        div className = "pb-3" >
-        <
-        GoogleLogin clientId = { `${process.env.REACT_APP_GOOGLE_CLIENT_ID}` }
-        onSuccess = { responseGoogle }
-        onFailure = { responseGoogle }
-        render = {
-            (renderProps) => ( <
-                button onClick = { renderProps.onClick }
-                disabled = { renderProps.disabled }
-                className = "btn btn-danger btn-lg btn-block" >
-                <
-                i className = "fab fa-google pr-2" > < /i> Login with Google{" "} <
-                /button>
-            )
-        }
-        cookiePolicy = { "single_host_origin" }
-        />{" "} <
-        /div>
+    return (
+        <div className="pb-3">
+            <GoogleLogin
+                clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                render={renderProps => (
+                    <button
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}
+                        className="btn btn-danger btn-lg btn-block"
+                    >
+                        <i className="fab fa-google pr-2"></i> Login with Google
+                    </button>
+                )}
+                cookiePolicy={'single_host_origin'}
+            />
+        </div>
     );
 };
 
