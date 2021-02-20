33914792d833a35737c003df373d4585089f311c
Mithun raam <mithunmaha1999@gmail.com>
2021-02-20 21:42:17 +0530

    latest

--- app.js
+++ app.js
@@ -51,12 +51,19 @@ app.use('/api', orderRoutes);
 
 
 
-if (process.env.NODE_ENV === 'production')
+// if (process.env.NODE_ENV === 'production')
 
-{
-    app.use(express.static('client/build'));
+// {
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join('build', 'index.html'));
     });
 }
 
