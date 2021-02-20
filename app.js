5e3b345778c7e708bac9d7dca6586915b0356552
Mithun raam <mithunmaha1999@gmail.com>
2021-02-20 22:05:29 +0530

    latest

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const port = process.env.port || 5000;
-app.set('port', (process.env.port || 5000));
+const port = process.env.PORT || 5000;
+app.set('port', (process.env.PORT || 5000));
 app.listen(port, () => console.log(`Example app listening on port port!`));
