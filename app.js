2a107f60c33a6a582c9dacce631e5ce865955b95
Mithun raam <mithunmaha1999@gmail.com>
2021-02-20 20:37:16 +0530

    new update

--- app.js
+++ app.js
@@ -50,16 +50,12 @@ app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
 
 
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
-if (process.env.NODE_ENV === 'production')
 
-{
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
-
-const port = process.env.port || 5000;
-app.set('port', (process.env.port || 5000));
+const port = process.env.PORT || 5000;
+app.set('port', (process.env.PORT || 5000));
 app.listen(port, () => console.log(`Example app listening on port port!`));
