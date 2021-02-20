94621a783936a18570455206dfc41a69bd05e6f7
Mithun raam <mithunmaha1999@gmail.com>
2021-02-20 15:38:27 +0530

    latest

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const orderRoutes = require('./routes/order');
 
 
 
-const app = express();
 app.use(compression());
 
 //db connection
@@ -55,14 +54,11 @@ app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
 
 
-if (process.env.NODE_ENV === 'production')
+app.use(express.static(path.join(__dirname, 'build')));
 
-{
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 
 const port = process.env.PORT || 8000;
