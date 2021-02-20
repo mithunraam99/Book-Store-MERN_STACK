50601a368080c416239c81f3015db6c5c5952c30
Mithun raam <mithunmaha1999@gmail.com>
2021-02-21 02:35:59 +0530

    last

--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -4,7 +4,7 @@ import { signout, isAuthenticated } from "../auth";
 import { itemTotal } from "./cartHelper";
 import $ from "jquery";
 import "../nav.scss";
-import { Button } from "react-bootstrap";
+import { Button } from 'react-bootstrap'
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -29,150 +29,129 @@ const isActive = (history, path) => {
 //   });
 // });
 
-const Menu = ({ history }) => ( <
-    div >
-    <
-    nav className = "navbar navbar-expand-md navbar-dark bg-dark" >
-    <
-    Link className = "navbar-brand hoverable"
-    id = "len1"
-    style = { isActive(history, "/") }
-    to = "/" >
-    <
-    i className = "fas fa-dumpster-fire fa-lg" > < /i> <
-    /Link> <
-    button className = "navbar-toggler"
-    data - toggle = "collapse"
-    data - target = "#navMenu" >
-    <
-    span className = "navbar-toggler-icon" > < /span> <
-    /button> <
-    div className = "collapse navbar-collapse"
-    id = "navMenu" >
-    <
-    ul className = "navbar-nav ml-auto" >
-    <
-    li className = "nav-item mr-2" >
-    <
-    Link className = "nav-link hoverable"
-    id = "len2"
-    style = { isActive(history, "/shop") }
-    to = "/shop" >
-    <
-    i className = "fas fa-book pr-2" > < /i> <
-    span className = "coll" > Shop < /span> <
-    /Link> <
-    /li>
+const Menu = ({ history }) => (
+    <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <Link
+                className="navbar-brand hoverable"
+                id="len1"
+                style={isActive(history, "/")}
+                to="/"
+            >
+                <i className="fas fa-dumpster-fire fa-lg"></i>
+            </Link>
+            <button
+                className="navbar-toggler"
+                data-toggle="collapse"
+                data-target="#navMenu"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navMenu">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item mr-2">
+                        <Link
+                            className="nav-link hoverable"
+                            id="len2"
+                            style={isActive(history, "/shop")}
+                            to="/shop"
+                        >
+                            <i className="fas fa-book pr-2"> </i><span className="coll">Shop</span>
+                        </Link>
+                    </li>
 
-    <
-    li className = "nav-item mr-2 " >
-    <
-    Link className = "nav-link hoverable"
-    id = "len3"
-    style = { isActive(history, "/cart") }
-    to = "/cart" >
-    <
-    i className = "fas fa-cart-plus" > < /i>{" "} <
-    sup >
-    <
-    small className = "cart-badge" > { itemTotal() } < /small> <
-    /sup> <
-    /Link> <
-    /li>
+                    <li className="nav-item mr-2 ">
+                        <Link
+                            className="nav-link hoverable"
+                            id="len3"
+                            style={isActive(history, "/cart")}
+                            to="/cart"
+                        >
+                            <i className="fas fa-cart-plus"></i>{" "}
+                            <sup>
+                                <small className="cart-badge">{itemTotal()}</small>
+                            </sup>
+                        </Link>
+                    </li>
 
-    {
-        isAuthenticated() && isAuthenticated().user.role === 0 && ( <
-            li className = "nav-item mr-2" >
-            <
-            Link className = "nav-link hoverable"
-            id = "len4"
-            style = { isActive(history, "/user/dashboard") }
-            to = "/user/dashboard" >
-            <
-            i className = "fas fa-user pr-2" > < /i>
-            Dashboard <
-            /Link> <
-            /li>
-        )
-    }
+                    {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                        <li className="nav-item mr-2">
+                            <Link
+                                className="nav-link hoverable"
+                                id="len4"
+                                style={isActive(history, "/user/dashboard")}
+                                to="/user/dashboard"
+                            >
+                                <i className="fas fa-user pr-2"></i>
+                Dashboard
+              </Link>
+                        </li>
+                    )}
 
-    {
-        isAuthenticated() && isAuthenticated().user.role === 1 && ( <
-            li className = "nav-item mr-2" >
-            <
-            Link className = "nav-link hoverable"
-            id = "len4"
-            style = { isActive(history, "/admin/dashboard") }
-            to = "/admin/dashboard" >
-            <
-            i className = "fas fa-user pr-2" > < /i> <
-            span className = "coll" > Dashboard < /span> <
-            /Link> <
-            /li>
-        )
-    }
+                    {isAuthenticated() && isAuthenticated().user.role === 1 && (
+                        <li className="nav-item mr-2">
+                            <Link
+                                className="nav-link hoverable"
+                                id="len4"
+                                style={isActive(history, "/admin/dashboard")}
+                                to="/admin/dashboard"
+                            >
+                                <i className="fas fa-user pr-2"></i>
+                                <span className="coll">Dashboard</span>
+                            </Link>
+                        </li>
+                    )}
 
-    {
-        !isAuthenticated() && ( <
-            Fragment >
-            <
-            li className = "nav-item mr-2" >
-            <
-            Link className = "nav-link hoverable"
-            id = "len5"
-            style = { isActive(history, "/signin") }
-            to = "/signin" >
-            <
-            i className = "fas fa-sign-in-alt pr-2" > < /i> <
-            span className = "coll" > Signin < /span> <
-            /Link> <
-            /li>
+                    {!isAuthenticated() && (
+                        <Fragment>
+                            <li className="nav-item mr-2">
+                                <Link
+                                    className="nav-link hoverable"
+                                    id="len5"
+                                    style={isActive(history, "/signin")}
+                                    to="/signin"
+                                >
+                                    <i className="fas fa-sign-in-alt pr-2"></i>
+                                    <span className="coll">Signin</span>
+                                </Link>
+                            </li>
 
-            <
-            li className = "nav-item mr-2" >
-            <
-            Link className = "nav-link hoverable"
-            id = "len6"
-            style = { isActive(history, "/signup") }
-            to = "/signup" >
-            <
-            i class = "fa fa-sign-out pr-2"
-            aria - hidden = "true" > < /i> <
-            span className = "coll" > Signup < /span> <
-            /Link> <
-            /li> <
-            /Fragment>
-        )
-    }
+                            <li className="nav-item mr-2">
+                                <Link
+                                    className="nav-link hoverable"
+                                    id="len6"
+                                    style={isActive(history, "/signup")}
+                                    to="/signup"
+                                >
+                                    <i class="fa fa-sign-out pr-2" aria-hidden="true">
+                                    </i>
+                                    <span className="coll">Signup</span>
+                                </Link>
+                            </li>
+                        </Fragment>
+                    )}
 
-    {
-        isAuthenticated() && ( <
-            li className = "nav-item " >
-            <
-            span className = "nav-link hoverable"
-            id = "len7"
-            style = {
-                { cursor: "pointer", color: "#ffffff" } }
-            onClick = {
-                () =>
-                signout(() => {
-                    history.push("/");
-                })
-            } >
-            <
-            span className = "coll" >
-            <
-            i className = "fas fa-sign-out-alt pr-2" > < /i>
-            Signout <
-            /span> <
-            /span> <
-            /li>
-        )
-    } <
-    /ul> <
-    /div> <
-    /nav> <
-    /div>
+                    {isAuthenticated() && (
+                        <li className="nav-item ">
+                            <span
+                                className="nav-link hoverable"
+                                id="len7"
+                                style={{ cursor: "pointer", color: "#ffffff" }}
+                                onClick={() =>
+                                    signout(() => {
+                                        history.push("/");
+                                    })
+                                }
+                            ><span className="coll">
+                                    <i className="fas fa-sign-out-alt pr-2"></i>
+                Signout</span>
+                            </span>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </nav>
+    </div>
 );
 
 export default withRouter(Menu);
